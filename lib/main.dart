c43fc51bd69fa17e07ec8ef50548d5f3027ba41d
Richard <rdobmk@gmail.com>
2024-08-09 23:52:49 +0200

    Set the app

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const String appTitle = 'Flutter layout demo';
+
+    return MaterialApp(
+      title: appTitle,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(appTitle),
+        ),
+        body: Center(
+          child: Text('Hellow Richard'),
+        ),
+      ),
+    );
+  }
+}
